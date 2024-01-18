8f20ba509e597b4980ddf5650c3f392bd3ea7fc2
Maxime THIZEAU <contact.thizeau@gmail.com>
2024-01-18 11:11:52 +0100

    comment zsh setup on install

--- install.sh
+++ install.sh
@@ -7,8 +7,8 @@ stow -R tmux
 stow -R .tmux
 stow -R zsh
 
-# add zsh as a login shell
-command -v zsh | sudo tee -a /etc/shells
+# # add zsh as a login shell
+# command -v zsh | sudo tee -a /etc/shells
 
-# use zsh as default shell
-sudo chsh -s $(which zsh) $USER
+# # use zsh as default shell
+# sudo chsh -s $(which zsh) $USER
